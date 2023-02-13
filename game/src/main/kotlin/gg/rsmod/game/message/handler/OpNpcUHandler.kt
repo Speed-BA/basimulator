05409b0810a32e9619c424832b0447799cfa1739
starfish671 <104892228+starfish671@users.noreply.github.com>
2023-02-14 10:00:03 +1030

    Update OpNpcUHandler.kt
    Revert comment and debug logging for OpNpcU

--- game/src/main/kotlin/gg/rsmod/game/message/handler/OpNpcUHandler.kt
+++ game/src/main/kotlin/gg/rsmod/game/message/handler/OpNpcUHandler.kt
@@ -1,6 +1,5 @@
 package gg.rsmod.game.message.handler
 
-import gg.rsmod.game.Server.Companion.logger
 import gg.rsmod.game.action.PawnPathAction
 import gg.rsmod.game.message.MessageHandler
 import gg.rsmod.game.message.impl.OpNpcUMessage
@@ -15,13 +14,10 @@ import java.lang.ref.WeakReference
 
 /**
  * @author Tom <rspsmods@gmail.com>
- *
- *     Does not work, check OpNpcTHandler for explanation
  */
 class OpNpcUHandler : MessageHandler<OpNpcUMessage> {
 
     override fun handle(client: Client, world: World, message: OpNpcUMessage) {
-        logger.info(message.toString())
         val index = message.npcIndex
         val npc = world.npcs[index] ?: return
 
